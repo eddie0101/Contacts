b6ce503a1b47394679b75393be6665141539f037
Eduard <ghiuzaneduard@gmail.com>
2022-10-25 15:23:58 +0300

    added fields and constructor

--- models/ContactManager.java
+++ models/ContactManager.java
@@ -1,3 +1,13 @@
+package models;
+
+import java.util.ArrayList;
+
 public class ContactManager {
+    private ArrayList<Contact> contacts;
+
+    public ContactManager() {
+        contacts = new ArrayList<Contact>();
+    }
+
     
 }

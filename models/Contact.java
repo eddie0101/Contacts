708d61cc54ffcd3f5e9e3feb4ddf143b6db1ff96
Eduard <ghiuzaneduard@gmail.com>
2022-10-25 12:56:26 +0300

    made constructor

--- models/Contact.java
+++ models/Contact.java
@@ -1,3 +1,28 @@
+package models;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Contact {
-    
+    private String name;
+    private String phoneNumber;
+    private String birthDate;
+    private int age;
+
+    public Contact(String name, String phoneNumber, String birthDate) throws ParseException {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+        this.age = toAge(birthDate);
+    }
+
+    private int toAge(String birthDate) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        Date toDate = formatter.parse(birthDate);
+        long toMilli = toDate.getTime();
+        long diff = new Date().getTime() - toMilli;
+        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) / 365;
+    }
 }
